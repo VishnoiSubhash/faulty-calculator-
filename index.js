4dabff579b0713dbd5cf96d67485f6bd24236995
Subhash Vishnoi <subhashvishnoi99@gmail.com>
2024-09-12 23:48:55 +0530

    faulty calculator

---
+++ index.js
@@ -0,0 +1,33 @@
+/*create  a faulty calculator using Javascript
+
++ ----> -
+- ----> /
+* ----->+
+/ ---->**
+
+*/
+
+let random = Math.random()
+console.log(random)
+let a = prompt("Enter first number ")
+let c = prompt("Enter operation")
+let b = prompt("Enter second number ")
+
+
+let obj ={
+    "+" : "-",
+    "-" : "/",
+    "*" : "+",
+    "/" : "*",
+
+}
+ if (random > 0.1) {
+    //perform correct calculation
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    
+ } else {
+    //perform wrong calculation
+    c = obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    
+ }
